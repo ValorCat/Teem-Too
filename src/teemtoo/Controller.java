61da6dd3455bfaf67bd41113dab7a11c5f72bb5f
ValorCat <amorrell314@gmail.com>
2017-11-17 13:18:07 -0500

    Create clock, menu button, and arrow buttons

--- src/teemtoo/Controller.java
+++ src/teemtoo/Controller.java
@@ -1,11 +1,68 @@
 package teemtoo;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
+
 /**
  * @author Anthony Morrell
  * @since 11/17/2017
  */
-public class Controller {
+public class Controller implements Initializable {
 
-    // todo
+    private static final String TIME_FORMAT = "h:mm a";
+
+    @FXML private Label clock;
+    @FXML private Button menuButton;
+    @FXML private Button leftButton;
+    @FXML private Button rightButton;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        setupClock();
+        menuButton.setGraphic(getImage("hamburger.png", 30, 30));
+    }
+
+    public void moveLeft() {
+
+    }
+
+    public void moveRight() {
+
+    }
+
+    private void setupClock() {
+        // todo ensure clock displays correctly when hour has 2 digits
+        DateTimeFormatter format = DateTimeFormatter.ofPattern(TIME_FORMAT);
+        KeyFrame frame1 = new KeyFrame(Duration.seconds(0), e -> clock.setText(LocalTime.now().format(format)));
+        KeyFrame frame2 = new KeyFrame(Duration.seconds(1));
+        Timeline timeline = new Timeline(frame1, frame2);
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+    }
+
+    private static ImageView getImage(String name, int width, int height) {
+        ImageView view = new ImageView(new Image(getResource(name)));
+        view.setFitWidth(width);
+        view.setFitHeight(height);
+        return view;
+    }
+
+    private static InputStream getResource(String name) {
+        return Controller.class.getResourceAsStream(name);
+    }
 
 }
--- src/teemtoo/Main.java
+++ src/teemtoo/Main.java
@@ -1,6 +1,9 @@
 package teemtoo;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 /**
@@ -9,9 +12,16 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
+    private static final int WIDTH = 400, HEIGHT = 250;
+    private static final String FXML_NAME = "activity-tracker.fxml";
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // todo
+        Pane window = FXMLLoader.load(Main.class.getResource(FXML_NAME));
+        Scene scene = new Scene(window, WIDTH, HEIGHT);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
 
 }
